e125a04e95d938a37410c91e4562ca1f88914ccb
Ricardo.Ayala <Ricardo.ayala-g@mail.escuelaing.edu.co>
2025-10-08 20:15:17 -0500

    inicio

---
+++ src/test/java/com/dinosurio_G/Back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinosurio_G.Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
